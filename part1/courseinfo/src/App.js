7ae98b13ef645048fdf3c90ef47b2cbf48f262ad
Abu Abdillah <olamide14044@gmail.com>
2022-01-20 12:27:14 +0100

    Completeing exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,13 +8,23 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
         {props.part} {props.exercise}
       </p>
     </>
+  )
+}
+
+const Content = ({part1, exercise1, part2, exercise2, part3, exercise3}) => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercise1} />
+      <Part part={part2} exercise={exercise2} />
+      <Part part={part3} exercise={exercise3} />
+    </div>
   )
 }
 
@@ -39,9 +49,11 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercise={exercise1} />
-      <Content part={part2} exercise={exercise2} />
-      <Content part={part3} exercise={exercise3} />
+      <Content 
+      part1={part1} part2={part2} part3={part3} 
+      exercise1={exercise1} 
+      exercise2={exercise2}
+      exercise3={exercise3}/>
       <Total total={exercise1 + exercise2 + exercise3}/>
     </div>
   )
